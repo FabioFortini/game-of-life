a860b6e4a972b273e1080a85032a8d8c8bae340e
fabiofortini <fabio.fortini@consultant.chili.com>
2022-05-05 09:21:52 +0200

    calculating neighbvours top row

--- src/main/kotlin/Grid.kt
+++ src/main/kotlin/Grid.kt
@@ -58,8 +58,10 @@ class Grid(val rows: Int = 10, val cols: Int = 10, aliveCells: List<Pair<Int, In
         return string
     }
 
-    fun getNeighboursAlive(x: Int, y: Int): Int {
-        return 0
+    fun getNeighboursAlive(row: Int, col: Int): Int {
+        return getCell(row - 1, col - 1).status +
+                getCell(row - 1, col).status +
+                getCell(row - 1, col + 1).status
 //        return getCell(x - 1, y - 1).status +
 //                getCell(x, y - 1).status +
 //                getCell(x + 1, y - 1).status
